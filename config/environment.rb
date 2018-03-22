dede3b556f8c1ffac59479c20c15faf33d04d82c
Alex Dolski <alexd@n-ary.net>
2018-03-22 09:25:41 -0500

    Send log output to stdout in dev and test environments

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.logger = ActiveSupport::Logger.new(STDOUT) unless Rails.env.production?
+
 # Initialize the Rails application.
 Rails.application.initialize!
