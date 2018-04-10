cf3c7f1170397909aae9a057acd7c8035f738f4a
Alex Dolski <alexd@n-ary.net>
2018-04-10 10:17:43 -0500

    Move the shibboleth host into an environment variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 # We need a config/shibboleth.yml file with the options
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    opts = YAML.load_file(File.join(Rails.root, 'config', 'shibboleth.yml'))[Rails.env]
+    opts = YAML.load(
+        ERB.new(
+            File.read(
+                File.join(Rails.root, 'config', 'shibboleth.yml'))).result)[Rails.env]
     provider :shibboleth, opts.symbolize_keys
     Metaslurp::Application.shibboleth_host = opts['host']
   else
