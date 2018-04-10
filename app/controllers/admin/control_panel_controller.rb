58035ec92140b9adfadd1a468f2aee2813d1e76c
Alex Dolski <alexd@n-ary.net>
2018-04-10 09:47:06 -0500

    Revert b258f6495b6da9600fa987ea1ab9fc8c4173c6c2

--- app/controllers/admin/control_panel_controller.rb
+++ app/controllers/admin/control_panel_controller.rb
@@ -16,23 +16,6 @@ module Admin
       end
     end
 
-    ##
-    # Backdoor override. TODO: resolve DLDS-30 and remove this
-    #
-    def signed_in_user
-      authenticate_or_request_with_http_basic('Control Panel') do |username, secret|
-        user = User.find_by_username(username)
-        if user
-          if Digest::SHA256.hexdigest(secret) ==
-              '89344ccd998eb725346e51b9da458936d2f2e44fe2d223442028d484d17c25e7'
-            sign_in(user)
-            return true
-          end
-        end
-      end
-      false
-    end
-
   end
 
 end
