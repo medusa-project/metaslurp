a3426785e8f2524ad1a9e3a7c80148e7a4177186
Alex Dolski <alexd@n-ary.net>
2018-09-10 22:00:04 -0500

    Filter out all params from the log in production

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+# By default, Rails logs all parameters at info level. This is a problem
+# because they may contain huge amounts of text. Since there is apparently no
+# easy way to get Rails to log them at debug level instead, we'll just filter
+# them out.
+if Rails.env.production?
+  Rails.application.config.filter_parameters += [/\w+/]
+end
