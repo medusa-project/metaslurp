baaa98f43a933f8db906833ce73a566c481dada6
Alex Dolski <alexd@n-ary.net>
2019-07-26 15:56:44 -0500

    Replace the docker subdirectory with the rails-container-scripts submodule

--- docker/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-source docker/env.sh
-
-docker build -f docker/Dockerfile \
-    --build-arg time_zone=$TIME_ZONE \
-    -t $APP_NAME \
-    .
--- docker/docker-run-webapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Runs the web app locally.
-
-source docker/env.sh
-
-docker run -p 3000:3000 -it --env-file docker/env-dev.list $APP_NAME
