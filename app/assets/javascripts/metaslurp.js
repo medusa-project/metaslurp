medusa-project/metaslurp

commit 7dac727b8e56719e614a932e53f45e2005ab68e1

FilterField removes event listeners before adding them

app/assets/javascripts/metaslurp.js
@@ -97,7 +97,7 @@ var Application = {
     FilterField: function() {
         var INPUT_DELAY_MSEC = 500;
 
-        $('form.dl-filter').submit(function () {
+        $('form.dl-filter').off().submit(function () {
             $.get(this.action, $(this).serialize(), null, 'script');
             $(this).nextAll('input').addClass('active');
             return false;
@@ -117,7 +117,7 @@ var Application = {
 
         var input_timer;
         // When text is typed in the filter field...
-        $('form.dl-filter input').on('keyup', function () {
+        $('form.dl-filter input').off().on('keyup', function () {
             // Reset the typing-delay counter.
             clearTimeout(input_timer);
 
@@ -129,7 +129,7 @@ var Application = {
         // When form controls accompanying the filter field are changed,
         // resubmit the form via AJAX.
         $('form.dl-filter select, ' +
-            'form.dl-filter input[type=radio]').on('change', function() {
+            'form.dl-filter input[type=radio]').off().on('change', function() {
             submitForm();
         });
     }

commit 2c7b9c32a1fc97c3495346db9ac86ab02615da13

Fix bug introduced in 7dac727

app/assets/javascripts/metaslurp.js
@@ -97,7 +97,7 @@ var Application = {
     FilterField: function() {
         var INPUT_DELAY_MSEC = 500;
 
-        $('form.dl-filter').off().submit(function () {
+        $('form.dl-filter').submit(function () {
             $.get(this.action, $(this).serialize(), null, 'script');
             $(this).nextAll('input').addClass('active');
             return false;
@@ -117,7 +117,7 @@ var Application = {
 
         var input_timer;
         // When text is typed in the filter field...
-        $('form.dl-filter input').off().on('keyup', function () {
+        $('form.dl-filter input').off('keyup').on('keyup', function () {
             // Reset the typing-delay counter.
             clearTimeout(input_timer);
 
@@ -129,7 +129,7 @@ var Application = {
         // When form controls accompanying the filter field are changed,
         // resubmit the form via AJAX.
         $('form.dl-filter select, ' +
-            'form.dl-filter input[type=radio]').off().on('change', function() {
+            'form.dl-filter input[type=radio]').off('change').on('change', function() {
             submitForm();
         });
     }