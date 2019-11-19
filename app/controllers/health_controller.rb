a9428a93c5efe9ae9a9a4d5251205a5c6ffd61da
Alex Dolski <alexd@n-ary.net>
2019-11-19 14:11:26 -0600

    Health check touches the database and Elasticsearch

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,6 +1,15 @@
 class HealthController < ApplicationController
 
+  ##
+  # Responds to GET /health
+  #
   def index
+    # touch the database
+    Harvest.count
+
+    # touch Elasticsearch
+    ItemFinder.new.aggregations(false).limit(0).count
+
     render plain: 'OK'
   end
 
