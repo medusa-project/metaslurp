9569f795fed88c040a2c96715b6e1adecd19b005
Alex Dolski <alexd@n-ary.net>
2019-11-26 12:58:19 -0600

    Don't check the database in demo

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,8 +4,9 @@ class HealthController < ApplicationController
   # Responds to GET /health
   #
   def index
-    # touch the database
-    Harvest.count
+    # Touch the database... except in demo, where the database is running in
+    # Aurora and costs nothing during idle periods.
+    Harvest.count unless Rails.env.demo?
 
     # touch Elasticsearch
     ItemFinder.new.aggregations(false).limit(0).count
