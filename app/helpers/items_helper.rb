medusa-project/metaslurp

commit 994b427994342ae32aafd270552db32481dff24a

Update Rails & scars-bootstrap-theme

app/assets/images/thumbnail-spinner.svg
@@ -1,1 +1,0 @@
-<svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background-image: none; background-position: initial initial; background-repeat: initial initial;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#f0f0f0" stroke-width="10" r="25" stroke-dasharray="117.80972450961724 41.269908169872416"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="5s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>

app/helpers/items_helper.rb
@@ -87,9 +87,9 @@ module ItemsHelper
       end
 
       unless thumbnail_is_local?(item)
-        # N.B.: this was made by https://loading.io with the following settings:
-        # rolling, color: #cacaca, radius: 25, stroke width: 10, speed: 5, size: 150
-        html <<     image_tag('thumbnail-spinner.svg', class: 'dl-load-indicator')
+        html << '<div class="spinner-border" role="status">'
+        html <<   '<span class="sr-only">Loading...</span>'
+        html << '</div>'
       end
       html <<   '</div>'
       html <<   '<div class="media-body">'

Gemfile
@@ -3,10 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+gem "autoprefixer-rails" # required to avoid some bootstrap CSS weirdness
 gem 'aws-sdk-ecs', '~> 1' # used to trigger harvests
-gem 'bootstrap', '~> 4.3'
-gem 'scars-bootstrap-theme', github: 'medusa-project/scars-bootstrap-theme'
 gem 'faraday'
+gem "font-awesome-sass", "~> 5.6"
 gem 'jbuilder'
 gem 'jquery-rails'
 gem 'js_cookie_rails'
@@ -14,10 +14,13 @@ gem 'local_time'
 gem 'marc-dates', git: 'https://github.com/medusa-project/marc-dates.git'
 gem 'mini_racer', platforms: :ruby
 gem 'omniauth'
+gem "omniauth-rails_csrf_protection"
 gem 'omniauth-shibboleth'
 gem 'pg'
-gem 'rails', '~> 6.0.1'
+gem 'rails', '~> 6.0.3'
 gem 'sassc'
+gem 'scars-bootstrap-theme', github: 'medusa-project/scars-bootstrap-theme',
+    branch: 'release/bootstrap-4.4'
 gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do

commit 937afad4b70ed25a94a90eade1e57b93c5446bb0

Fix a regression from 994b427994342ae32aafd270552db32481dff24a

app/assets/images/thumbnail-spinner.svg
@@ -1,0 +1,1 @@
+<svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background-image: none; background-position: initial initial; background-repeat: initial initial;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#f0f0f0" stroke-width="10" r="25" stroke-dasharray="117.80972450961724 41.269908169872416"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="5s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>

app/helpers/items_helper.rb
@@ -87,9 +87,9 @@ module ItemsHelper
       end
 
       unless thumbnail_is_local?(item)
-        html << '<div class="spinner-border" role="status">'
-        html <<   '<span class="sr-only">Loading...</span>'
-        html << '</div>'
+        # N.B.: this was made by https://loading.io with the following settings:
+        # rolling, color: #cacaca, radius: 25, stroke width: 10, speed: 5, size: 150
+        html <<     image_tag('thumbnail-spinner.svg', class: 'dl-load-indicator')
       end
       html <<   '</div>'
       html <<   '<div class="media-body">'