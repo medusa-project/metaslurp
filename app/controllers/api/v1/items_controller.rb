medusa-project/metaslurp

commit 2b4e55d94af8f3b480c26636519de383e26b26ed

Update request_json to work with a request.body of Tempfile type

app/controllers/api/v1/items_controller.rb
@@ -43,8 +43,13 @@ module Api
 
       def request_json
         entity = request.body
-        entity = entity.is_a?(StringIO) ? entity.string : entity.to_s
-
+        if entity.is_a?(StringIO)
+          entity = entity.string
+        elsif entity.is_a?(Tempfile)
+          entity = File.read(entity)
+        else
+          entity = entity.to_s
+        end
         json = JSON.parse(entity)
 
         # Validate the harvest key

app/controllers/api/v1/harvests_controller.rb
@@ -16,7 +16,7 @@ module Api
           harvest.update_from_json(json)
           LOGGER.debug('Created harvest %s', harvest)
         rescue ArgumentError, JSON::ParserError => e
-          LOGGER.debug('Invalid: %s', json)
+          LOGGER.debug('%s: %s', e, request.body)
           render plain: e.message, status: :bad_request
         rescue IOError => e
           LOGGER.error(e)
@@ -54,7 +54,13 @@ module Api
 
       def request_json
         entity = request.body
-        entity = entity.is_a?(StringIO) ? entity.string : entity.to_s
+        if entity.is_a?(StringIO)
+          entity = entity.string
+        elsif entity.is_a?(Tempfile)
+          entity = File.read(entity)
+        else
+          entity = entity.to_s
+        end
         json = JSON.parse(entity)
         # API exposes `service_key` but the Harvest requires
         # `content_service_id`.

commit b06a91fe2a3fc5646672de1fc7c25c9fd25ef30b

Fix a bug in request_json() introduced in 2b4e55d94af8f3b480c26636519de383e26b26ed

app/controllers/api/v1/items_controller.rb
@@ -43,12 +43,8 @@ module Api
 
       def request_json
         entity = request.body
-        if entity.is_a?(StringIO)
-          entity = entity.string
-        elsif entity.is_a?(Tempfile)
-          entity = File.read(entity)
-        else
-          entity = entity.to_s
+        unless entity.is_a?(String)
+          entity = entity.read
         end
         json = JSON.parse(entity)
 