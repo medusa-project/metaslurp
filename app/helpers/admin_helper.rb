ec9bf076464f80361539a5cb650dbfd3a045fea2
Alex Dolski <alexd@n-ary.net>
2020-05-21 11:36:10 -0500

    Replace table layout with a responsive form

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,37 +1,6 @@
 module AdminHelper
 
   ##
-  # @param option_keys [Enumerable<Hash<String,String>>] Enumerable of hashes
-  #                    with :label, :key, and :type keys.
-  # @return [String] HTML table element.
-  #
-  def admin_configuration_table_for(*option_keys)
-    html = '<table class="table">'
-    option_keys.each do |key|
-      html += '<tr><td>'
-      html += label_tag("configuration[#{key[:key]}]", key[:label])
-      html += '</td><td>'
-      case key[:type]
-        when :number
-          html += number_field_tag("configuration[#{key[:key]}]",
-                                   Option::string(key[:key]),
-                                   class: 'form-control')
-        when :textarea
-          html += text_area_tag("configuration[#{key[:key]}]",
-                                 Option::string(key[:key]),
-                                 class: 'form-control')
-        else
-          html += text_field_tag("configuration[#{key[:key]}]",
-                                 Option::string(key[:key]),
-                                 class: 'form-control')
-      end
-      html += '</td></tr>'
-    end
-    html += '</table>'
-    raw(html)
-  end
-
-  ##
   # @param status [Integer] One of the Harvest::Status constant values.
   # @return [String] HTML span element.
   #
