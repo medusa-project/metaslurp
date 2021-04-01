839b2ec7b60e6a204f21adb5405f7b62f38699e8
Alex Dolski <alexd@n-ary.net>
2021-04-01 10:06:34 -0500

    rescue_internal_server_error() sends an email to administrators

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -90,11 +90,11 @@ class ApplicationController < ActionController::Base
   def rescue_internal_server_error(exception)
     io = StringIO.new
     io << "Error on #{request.url}\n"
-    io << "Class: #{exception.class}\n"
+    io << "Class:   #{exception.class}\n"
     io << "Message: #{exception.message}\n"
-    io << "Time: #{Time.now.iso8601}\n"
-    io << "User: #{current_user.username}}\n" if current_user
-    io << "Stack Trace:\n"
+    io << "Time:    #{Time.now.iso8601}\n"
+    io << "User:    #{current_user.name}\n" if current_user
+    io << "\nStack Trace:\n"
     exception.backtrace.each do |line|
       io << line
       io << "\n"
@@ -102,6 +102,10 @@ class ApplicationController < ActionController::Base
 
     @message = io.string
     Rails.logger.error(@message)
+
+    unless Rails.env.development?
+      MetaslurpMailer.error(@message).deliver_now
+    end
 
     respond_to do |format|
       format.html do
