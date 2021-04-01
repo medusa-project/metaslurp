0be716efc6a8f413b81556eb5236b25fde73a082
Alex Dolski <alexd@n-ary.net>
2021-04-01 14:35:39 -0500

    Health check does not touch Elasticsearch

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -5,13 +5,12 @@ class HealthController < ApplicationController
   #
   def index
     # Touch the database... except in demo, where the database is running in
-    # Aurora and costs nothing during idle periods.
+    # Aurora and we can save money by letting it go idle.
     Harvest.count unless Rails.env.demo?
-
-    # touch Elasticsearch
-    ItemFinder.new.aggregations(false).limit(0).count
-
-    render plain: 'OK'
+  rescue => e
+    render plain: "RED: #{e}", status: :internal_server_error
+  else
+    render plain: 'GREEN'
   end
 
 end
