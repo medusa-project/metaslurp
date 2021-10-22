86f23557c54b8ffcfecd27f0a7221ae5b813244a
Alex Dolski <alexd@n-ary.net>
2021-10-22 15:02:44 -0500

    Add a button to purge cached images from the image server

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,10 +2,39 @@ module Admin
 
   class ItemsController < ControlPanelController
 
+    before_action :load_item
+
+    ##
+    # Purges any images relating to the item from the image server's cache.
+    #
+    # Responds to `PATCH /admin/items/:id/purge-cached-images` (XHR only)
+    #
+    def purge_cached_item_images
+      begin
+        @item.purge_cached_images
+      rescue => e
+        handle_error(e)
+      else
+        flash['success'] = 'All cached images associated with this item have '\
+                           'been purged.'
+      ensure
+        redirect_back fallback_location: admin_item_path(@item)
+      end
+    end
+
+    ##
+    # Responds to `GET /admin/items/:id`
+    #
     def show
-      @item = Item.find(params[:id])
       @indexed_document = Item.fetch_indexed_json(@item.id)
-      @boosts = Boost.all
+      @boosts           = Boost.all
+    end
+
+
+    private
+
+    def load_item
+      @item = Item.find(params[:id] || params[:item_id])
     end
 
   end
