1824cfe93068bbd43756b52b7fbca1068addeb26
Alex Dolski <alexd@n-ary.net>
2023-09-29 15:08:58 -0500

    Guard against empty uiuc_lib_ad configuration

--- config/initializers/uiuc_lib_ad.rb
+++ config/initializers/uiuc_lib_ad.rb
@@ -3,10 +3,11 @@
 require 'configuration'
 
 app_config = ::Configuration.instance
-
-UiucLibAd::Configuration.instance = UiucLibAd::Configuration.new(
-  user:     app_config.ad[:user],
-  password: app_config.ad[:password],
-  server:   app_config.ad[:server],
-  treebase: app_config.ad[:treebase]
-)
+if app_config.ad
+  UiucLibAd::Configuration.instance = UiucLibAd::Configuration.new(
+    user:     app_config.ad[:user],
+    password: app_config.ad[:password],
+    server:   app_config.ad[:server],
+    treebase: app_config.ad[:treebase]
+  )
+end
